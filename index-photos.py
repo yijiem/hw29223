76f65e819619f55af9609393d310e3505a7b48e6
Elaina Huang <64375965+yh0010@users.noreply.github.com>
2023-04-10 11:03:10 -0700

    Update index-photos.py

--- index-photos.py
+++ index-photos.py
@@ -1,63 +1,69 @@
 import json
 import boto3
-from aws_requests_auth.aws_auth import AWSRequestsAuth
+from opensearchpy import OpenSearch, RequestsHttpConnection, AWSV4SignerAuth 
 import time
-from elasticsearch import Elasticsearch, RequestsHttpConnection
-import requests
 import io
 import base64
-from base64 import decodestring
 
 def lambda_handler(event, context):
-    S3_details=event['Records'][0]['s3']
+    # print(event)
+    
     bucket=event['Records'][0]['s3']['bucket']['name']
     photo=event['Records'][0]['s3']['object']['key']
-    timeStamp=time.time()
+    # print(bucket,photo)
+    
     s3_client = boto3.client('s3')
-    s3_clientobj = s3_client.get_object(Bucket=bucket, Key=photo)
-    body=s3_clientobj['Body'].read().decode('utf-8')
-    image = base64.b64decode(body)        
     
-    
-    response=s3_client.delete_object(Bucket=bucket,Key=photo)
-    response=s3_client.put_object(Bucket=bucket, Body=image, Key=photo,ContentType='image/jpg')
-    
+    response = s3_client.get_object(Bucket=bucket, Key=photo)
+
+    img = response['Body'].read()
+    myObj = [base64.b64encode(img)]
+    return_json = str(myObj[0]) 
+    return_json = return_json.replace("b'","")          # replace this 'b'' is must to get absoulate image.
+    image = return_json.replace("'","")
+    print(image)
     
     client=boto3.client('rekognition')
-    response = client.detect_labels(
-    Image={'Bytes':image},
-    MaxLabels=10,
+    res = client.detect_labels(
+    Image={'Bytes':img},
+    MaxLabels=25,
     MinConfidence=80
     )
     
-    
-    labels=response['Labels']
+    labels=res['Labels']
     custom_labels=[]
+    timeStamp=time.time()
     for label in labels:
         custom_labels.append(label['Name'])
-   
-    format={
-        'objectKey':photo,
-        'bucket':bucket,
-        'createdTimeStamp':timeStamp,
-        'labels':custom_labels
-    }
+        
+    # print(custom_labels)
     
-    host="search-piper-es-2vi7inr55exmidpivownvkwgqu.us-east-1.es.amazonaws.com"
+    host="search-photosnew-tffirm2thjh6btphreva77wjhu.us-east-1.es.amazonaws.com"
+    region='us-east-1'
     
-    es_payload=json.dumps(format).encode("utf-8")
-    awsauth = AWSRequestsAuth(aws_access_key='',
-                      aws_secret_access_key='',
-                      aws_host=host,
-                      aws_region='us-east-1',
-                      aws_service='es')
+    credentials=boto3.Session().get_credentials()
+    auth=AWSV4SignerAuth(credentials, region)
+    print('\nauth')
+    print(auth)
     
-    esClient = Elasticsearch(
+    osClient = OpenSearch(
         hosts=[{'host': host, 'port':443}],
         use_ssl=True,
-        http_auth=awsauth,
+        http_auth=auth,
         verify_certs=True,
         connection_class=RequestsHttpConnection)
         
-    temp=esClient.index(index='photos', doc_type='photo', body=format)
+    index_body = {
+              'objectKey':photo,
+              'bucket':bucket,
+              'createdTimeStamp':timeStamp,
+              'labels':custom_labels
+        }
+    temp=osClient.index(index='photos', body=index_body)
     print(temp)
+    
+    return {
+            'status': 'True',
+            'statusCode': 200,
+            'encoded_image':image          # returning base64 of your image which in s3 bucket.
+    }
